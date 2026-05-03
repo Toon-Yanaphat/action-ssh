85265709f9ceb30cd2537746c93e7b187ff08d3a
Toon-Yanaphat <yanaput3559@gmail.com>
2026-05-04 01:01:35 +0700

    Update entrypoint.sh with new SSH script

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,13 +6,15 @@ USERNAME="$2"
 KEY="$3"
 PORT="$4"
 TIMEOUT="$5"
-SCRIPT="$6"
+ENVS="$6"
+SCRIPT="$7"
 
 # Write key to file
 echo "$KEY" > key.pem
 chmod 600 key.pem
 
-# Connect via SSH and run the script
+# Connect via SSH and run the script with environment variables
 ssh -i key.pem -o StrictHostKeyChecking=no -o ConnectTimeout="$TIMEOUT" -p "$PORT" "$USERNAME@$HOST" "bash -s" <<EOF
+$ENVS
 $SCRIPT
 EOF
